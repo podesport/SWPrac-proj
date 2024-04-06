5209fd1332298270324a3b57461b18cb069d4dbe
podesport <88339411+podesport@users.noreply.github.com>
2024-04-07 00:29:38 +0700

    implement user

---
+++ middleware/auth.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+// Protect routes
+exports.protect = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    // Make sure token exists
+    if (!token || token == 'null') {
+        return res.status(401).json({ success: false, message: 'Not authorized to access this route' });
+    }
+
+    try {
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        console.log(decoded);
+
+        req.user = await User.findById(decoded.id);
+
+        console.log(req.user);
+        next();
+    } catch (err) {
+        console.error(err.stack);
+
+        return res.status(401).json({ success: false, message: 'Not authorized to access this route' });
+    }
+};
+
+exports.authorize = (...roles) => {
+    return (req,res,next) =>{
+        if(!roles.includes(req.user.role)){
+            console.log(req.user);
+            return res.status(403).json({ success: false, message: `User role ${req.user.role} is not authorized to access this route`});
+        }
+        
+        next();
+    };
+};
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var cors = require('cors')
 app.use(cors())
 app.use('/api/v1/hotels', hotels);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
 
 process.on('unhandleRejection', (err, promise) => {
