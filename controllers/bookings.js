eb65aeba3995c47bbcd98faa036e1476a1e003e0
Stickman008 <arm.supakorn@gmail.com>
2024-04-11 19:40:49 +0700

    add booking template

---
+++ controllers/bookings.js
@@ -0,0 +1,28 @@
+const Booking = require('../models/Booking');
+
+
+// TODO
+exports.getBookings = async (req, res, next) => {
+
+}
+
+// TODO
+exports.getBooking = async (req, res, next) => {
+
+}
+
+// TODO
+exports.createBooking = async (req, res, next) => {
+
+}
+
+// TODO
+exports.updateBooking = async (req, res, next) => {
+    
+}
+
+
+// TODO
+exports.deleteBooking = async (req, res, next) => {
+    
+}
--- routes/bookings.js
+++ routes/bookings.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router()
 
-
+const {
+    getBookings,
+    getBooking,
+    createBooking,
+    updateBooking,
+    deleteBooking
+} = require('../controllers/bookings')
 const { protect, authorize } = require('../middleware/auth');
+
+router.route('/').get(getBookings).post(createBooking)
+router.route('/:id').get(getBooking).put(updateBooking).delete(deleteBooking)
+
+// router.route('/').get(protect, getBookings).post(protect, authorize('admin', 'user'), createBooking);
+// router.route('/:id').get(protect, getBooking).put(protect, authorize('admin', 'user'), updateBooking).delete(protect, authorize('admin', 'user'), deleteBooking);
+
 
 module.exports = router;
