24fe2afa0d41336a17e4d837aacb0da85dc0666f
Stickman008 <69195590+Stickman008@users.noreply.github.com>
2024-04-13 11:30:39 +0700

    update dummy booking controller

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -3,26 +3,26 @@ const Booking = require('../models/Booking');
 
 // TODO
 exports.getBookings = async (req, res, next) => {
-
+    res.status(200).send("OK");
 }
 
 // TODO
 exports.getBooking = async (req, res, next) => {
-
+    res.status(200).send("OK");
 }
 
 // TODO
 exports.createBooking = async (req, res, next) => {
-
+    res.status(200).send("OK");
 }
 
 // TODO
 exports.updateBooking = async (req, res, next) => {
-    
+    res.status(200).send("OK");
 }
 
 
 // TODO
 exports.deleteBooking = async (req, res, next) => {
-    
+    res.status(200).send("OK");
 }
