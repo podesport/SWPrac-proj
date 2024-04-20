3963df2ee6fc1c231c146e9e14cb95d4dda73964
Stickman008 <arm.supakorn@gmail.com>
2024-04-20 21:54:42 +0700

    add usecase diagram

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -1,28 +1,47 @@
+const jsPDF = require('jspdf');
 const Booking = require('../models/Booking');
-
 
 // TODO
 exports.getBookings = async (req, res, next) => {
-    res.status(200).send({"details": "OK"});
+    res.status(200).send({ "details": "OK" });
 }
 
 // TODO
 exports.getBooking = async (req, res, next) => {
-    res.status(200).send({"details": "OK"});
+    res.status(200).send({ "details": "OK" });
 }
 
 // TODO
 exports.createBooking = async (req, res, next) => {
-    res.status(200).send({"details": "OK"});
+    res.status(200).send({ "details": "OK" });
 }
 
 // TODO
 exports.updateBooking = async (req, res, next) => {
-    res.status(200).send({"details": "OK"});
+    res.status(200).send({ "details": "OK" });
 }
 
 
 // TODO
 exports.deleteBooking = async (req, res, next) => {
-    res.status(200).send({"details": "OK"});
+    res.status(200).send({ "details": "OK" });
+}
+
+
+function convertToPDF(bookingData) {
+    const doc = new jsPDF();
+    
+    doc.text("", 10, 10);
+    const pdfDataUri = doc.output('datauristring');
+    return pdfDataUri
+}
+// TODO
+exports.getBookingPDF = async (req, res, next) => {
+    res.set({
+        'Content-Type': 'application/pdf',
+        'Content-Disposition': 'attachment; filename="booking.pdf"'
+    });
+
+    const pdfDataUri = convertToPDF({ "test": "test" });
+    res.send(pdfDataUri);
 }
--- routes/bookings.js
+++ routes/bookings.js
@@ -6,13 +6,14 @@ const {
     getBooking,
     createBooking,
     updateBooking,
-    deleteBooking
+    deleteBooking,
+    getBookingPDF
 } = require('../controllers/bookings')
 const { protect, authorize } = require('../middleware/auth');
 
 router.route('/').get(getBookings).post(createBooking)
+router.route('/:id/pdf').get(getBookingPDF)
 router.route('/:id').get(getBooking).put(updateBooking).delete(deleteBooking)
-// router.route('/:id/download/pdf')
 
 // router.route('/').get(protect, getBookings).post(protect, authorize('admin', 'user'), createBooking);
 // router.route('/:id').get(protect, getBooking).put(protect, authorize('admin', 'user'), updateBooking).delete(protect, authorize('admin', 'user'), deleteBooking);
