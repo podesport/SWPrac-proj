a33189f08fd74a5648c45d45dd31558404bbdf10
Stickman008 <arm.supakorn@gmail.com>
2024-04-26 16:32:20 +0700

    add pdf endpoint

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -1,6 +1,10 @@
-const jsPDF = require('jspdf');
+const fs = require('fs');
+const path = require('path');
+const { PassThrough } = require('stream');
+const PDFDocument = require('pdfkit-table');
 const Booking = require('../models/Booking');
 const Hotel = require('../models/Hotel');
+const User = require('../models/User');
 
 //@desc Get all booking
 //@route GET /api/v1/booking
@@ -82,13 +86,13 @@ exports.createBooking = async (req, res, next) => {
 
         const nights = req.body.nights;
         if (!nights) {
-          throw "nights is required";
+            throw "nights is required";
         }
         if (nights < 1 || nights > 3) {
-          return res.status(400).json({
-            success: false,
-            message: `The nights number is invalid`,
-          });
+            return res.status(400).json({
+                success: false,
+                message: `The nights number is invalid`,
+            });
         }
         delete req.body.nights;
 
@@ -97,11 +101,11 @@ exports.createBooking = async (req, res, next) => {
         endDate.setDate(startDate.getDate() + nights);
         req.body.endDate = endDate;
         req.body.user = req.user.id;
-        req.body.hotel = req.params.hotelID ;
+        req.body.hotel = req.params.hotelID;
 
-        const cumulativeBooking = await Booking.find({user:req.user.id});
+        const cumulativeBooking = await Booking.find({ user: req.user.id });
         const existingBookings = await Booking.find({
-            user:req.user.id,
+            user: req.user.id,
             hotel: req.params.hotelID,
             $or: [
                 { startDate: { $lt: endDate }, endDate: { $gt: startDate } },
@@ -113,9 +117,9 @@ exports.createBooking = async (req, res, next) => {
                 succes: false,
                 message: `There is already a booking that overlaps with these dates.`
             });
-        } 
+        }
 
-        if (cumulativeBooking  >= 3 && req.user.role !== 'admin'){
+        if (cumulativeBooking >= 3 && req.user.role !== 'admin') {
             return res.status(400).json({
                 succes: false,
                 message: `User ID ${req.params.id} has 3 bookings ago and cannot bookings again.`
@@ -126,9 +130,9 @@ exports.createBooking = async (req, res, next) => {
         return res.status(200).json({
             succes: true,
             data: booking
-        });      
-    
-    }catch(err) {
+        });
+
+    } catch (err) {
         // console.log(err)
         return res.status(500).json({
             success: false,
@@ -165,7 +169,7 @@ exports.updateBooking = async (req, res, next) => {
             success: true,
             data: booking
         });
-    }catch(error){
+    } catch (error) {
         // console.log(error);
         return res.status(500).json({
             success: false,
@@ -186,7 +190,7 @@ exports.deleteBooking = async (req, res, next) => {
                 message: `No booking with the id of ${req.params.id}`,
             });
         }
-    
+
         if (booking.user.toString() !== req.user.id &&
             req.user.role !== "admin"
         ) {
@@ -209,22 +213,67 @@ exports.deleteBooking = async (req, res, next) => {
 
 exports.getBookingPDF = async (req, res, next) => {
     try {
-        // const booking = await Booking.findById(req.params.id).populate({
-        //     path: 'hotel',
-        //     select: "name province telephone"
-        // })
-        console.log(req.params.id);
-        const booking = await Booking.findById(req.params.id);
+        const booking = await Booking.findById(req.params.id).populate({
+            path: 'hotel',
+            select: "name address district province telephone"
+        });
         if (!booking) {
             return res.status(400).json({
                 succes: false,
                 message: `No booking with the id of ${req.params.id}`
             });
         }
-        res.status(200).json({
-            succes: true,
-            data: booking
-        })
+        const user = await User.findById(req.user.id);
+
+        // Create a pass-through stream to capture PDF content
+        const bufferStream = new PassThrough();
+        let pdfBuffer = Buffer.from('');
+
+        // Pipe the PDF content to the buffer stream
+        bufferStream.on('data', (chunk) => {
+            pdfBuffer = Buffer.concat([pdfBuffer, chunk]);
+        });
+
+        let doc = new PDFDocument({ margin: 30, size: "A4", bufferPages: true });
+        doc.pipe(res);
+
+        const userTable = {
+            title: "User Information",
+            headers: ["Key", "Value"],
+            rows: [
+                ['User ID:', user._id.toString()],
+                ['Name:', user.name],
+                ['Email:', user.email],
+                ['Telephone:', user.telephone]
+            ],
+        };
+        const bookingDateTable = {
+            title: "Booking Date Information",
+            headers: ["Key", "Value"],
+            rows: [
+                ['Booking Date:', booking.createAt],
+                ['Start Date:', booking.startDate],
+                ['End Date:', booking.endDate]
+            ],
+        };
+
+        const hotelTable = {
+            title: "Hotel Information",
+            headers: ["Key", "Value"],
+            rows: [
+                ['Hotel Name:', booking.hotel.name],
+                ['Hotel Address:', booking.hotel.address],
+                ['Hotel Province:', booking.hotel.province],
+                ['Hotel District:', booking.hotel.district],
+                ['Hotel Telephone:', booking.hotel.telephone]
+            ],
+        };
+        doc.font('Helvetica-Bold').fontSize(20).text('Booking Information', { align: 'center' }).moveDown();
+        await doc.table(userTable, {})
+        await doc.table(bookingDateTable, {})
+        await doc.table(hotelTable, {})
+
+        doc.end();
     } catch (err) {
         return res.status(500).json({
             succes: false,
